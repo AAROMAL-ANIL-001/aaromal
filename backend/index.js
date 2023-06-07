d59b53bc9a0de4c34be3b5f4352470532c9f858f
AAROMAL-ANIL-001 <aaromalanil001@gmail.com>
2023-06-08 01:14:31 +0530

    mongo string to local

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,7 @@ const app = express();
 app.use(express.json());
 
 mongoose
-  .connect(
-    "mongodb+srv://Aaromal:Upscarml123@cluster0.lmzxbce.mongodb.net/IROID"
-  )
+  .connect("mongodb://localhost:27017/iroid")
   .then(() => console.log("Db connected"))
   .catch(() => console.log("error in db"));
 
